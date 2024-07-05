bd0d061eb902a46f5b03805423cef5ee38084a1e
MRosTobiasson <117571587+MRosTobiasson@users.noreply.github.com>
2024-07-05 16:10:20 +0200

    added navbar and easy animations

---
+++ frontend/src/navbar/Navbar.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { ReactComponent as LinkedinLogo } from '../assets/images/linkedin-logo.svg';
+import { ReactComponent as GithubLogo } from '../assets/images/github-logo.svg';
+import { motion, AnimatePresence } from 'framer-motion';
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <motion.nav
+            className="navbar"
+            initial={{ y: -100 }}
+            animate={{ y: 0 }}
+            transition={{ type: 'spring', stiffness: 120 }}
+        >
+            <div className="navbar-logo">
+                <a href="#">MRT</a>
+            </div>
+            <ul className="navbar-links">
+                <motion.li
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.9 }}
+                >
+                    <NavLink to="/about-me" activeClassName="active">
+                        .aboutMe()
+                    </NavLink>
+                </motion.li>
+                <motion.li
+                    whileHover={{ scale: 1 }}
+                    whileTap={{ scale: 0.9 }}
+                >
+                    <a href="mailto:youremail@example.com" target="_blank" rel="noopener noreferrer">
+                        .contact()
+                    </a>
+                </motion.li>
+                <motion.li
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.9 }}
+                >
+                    <a href="https://www.linkedin.com/in/mikael-ros-tobiasson-3b632a243/" title='LinkedIn' target='_blank' rel="noopener noreferrer">
+                        <LinkedinLogo className="linkedinLogo" />
+                    </a>
+                </motion.li>
+                <motion.li
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.9 }}
+                >
+                    <a href="https://github.com/Mobiasson" title='GitHub' target='_blank' rel="noopener noreferrer">
+                        <GithubLogo className="githubLogo" />
+                    </a>
+                </motion.li>
+            </ul>
+        </motion.nav>
+    );
+}
+
+export default Navbar;
---
+++ frontend/src/pages/Home/HomePage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './HomePage.css';
+
+const Homepage = () => {
+    return (
+        <><div id="title">
+            <h1>MRT</h1>
+        </div>
+            <div id="name">
+                <h2>Mikael Ros Tobiasson</h2>
+            </div></>
+    );
+}
+
+export default Homepage;
